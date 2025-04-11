e6e3c6f119c2fa92442eec32943219124b0ec054
sbai12 <mouataz.sbai@esprit.tn>
2025-04-11 21:54:44 +0100

    Renamed demo to livraison and added new implementation

---
+++ backEnd/livraison/src/main/java/com/example/livraison/service/livraisonserv.java
@@ -0,0 +1,41 @@
+package com.example.livraison.service;
+
+import com.example.livraison.Repository.LivraisonRepo;
+import com.example.livraison.entity.Livraison;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+@AllArgsConstructor
+@Service
+
+public class livraisonserv implements ILivraisonServ{
+    private LivraisonRepo livraisonRepo;
+    @Override
+    public Livraison createLivraison(Livraison liv) {
+
+        return livraisonRepo.save(liv);
+    }
+
+    @Override
+    public List<Livraison> getAllLivraisons() {
+        return livraisonRepo.findAll();
+    }
+
+    @Override
+    public Optional<Livraison> getLivraisonyById(Long id) {
+        return livraisonRepo.findById(id);
+    }
+
+    @Override
+    public Livraison updateLivraison(Long id, Livraison liv) {
+        liv.setId(id);
+        return livraisonRepo.save(liv);
+    }
+
+    @Override
+    public void deleteLivraison(Long id) {
+        livraisonRepo.deleteById(id);
+    }
+}
